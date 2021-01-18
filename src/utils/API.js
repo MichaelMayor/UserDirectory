f7b017ad68f0466be2272871960d58ac4d5f4f6a
MichaelMayor <michael.davidge.mayor@gmail.com>
2021-01-18 14:28:28 -0500

    Added API call

---
+++ src/utils/API.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export default function getEmployees() {
+    return axios.get("https://randomuser.me/api/?results=250&nat=us&inc=name,email,phone,id,picture,dob")
+     ;
+  };
